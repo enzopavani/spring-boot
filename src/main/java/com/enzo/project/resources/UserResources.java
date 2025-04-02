789f4742fbc6453dc6e0702a8ac1291ad5490272
Enzo <tijoloenso@gmail.com>
2025-04-02 11:57:07 -0300

    User entity and resource

---
+++ src/main/java/com/enzo/project/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.enzo.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enzo.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Enzo", "enzo@email", "12345678", "password");
+		return ResponseEntity.ok().body(u);
+	}
+}
