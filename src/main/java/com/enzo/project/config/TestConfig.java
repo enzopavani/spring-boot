8dfe59d0c95d1a043e8c0eff0d6e0cacd706124d
Enzo <tijoloenso@gmail.com>
2025-04-15 20:20:19 -0300

    Payment, one-to-one association

--- src/main/java/com/enzo/project/config/TestConfig.java
+++ src/main/java/com/enzo/project/config/TestConfig.java
@@ -12,6 +12,7 @@ import com.enzo.project.entities.Product;
 import com.enzo.project.entities.Category;
 import com.enzo.project.entities.Order;
 import com.enzo.project.entities.OrderItem;
+import com.enzo.project.entities.Payment;
 import com.enzo.project.entities.User;
 import com.enzo.project.entities.enums.OrderStatus;
 import com.enzo.project.repositories.CategoryRepository;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
