d538a807e81358f1e275832ae84111ff7187d872
Enzo <tijoloenso@gmail.com>
2025-04-16 16:35:24 -0300

    Exception handling - delete

--- src/main/java/com/enzo/project/services/UserService.java
+++ src/main/java/com/enzo/project/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.enzo.project.entities.User;
 import com.enzo.project.repositories.UserRepository;
+import com.enzo.project.services.exceptions.DatabaseException;
 import com.enzo.project.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,9 +32,16 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			if(!repository.existsById(id)) {
+				throw new ResourceNotFoundException(id);
+			}
+			repository.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
